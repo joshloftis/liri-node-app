dc185388e41b3b3f77bed42ef1a8099668ea488e
Josh Loftis <jshlfts32@gmail.com>
2017-10-19 23:02:13 -0400

    Drying up some logic

--- liri.js
+++ liri.js
@@ -18,21 +18,27 @@ let app = {
     const params = ({screen_name: "sunbak885", count: 20});
       keys.twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
+        let allOutput = "";
         for (let i=0;i<tweets.length;i++) {
           const output = `-------------------------------\nThe following tweet was created on:\n${tweets[i].created_at} and reads:\n${tweets[i].text}\n`;
           console.log(output);
-          fs.appendFile("log.txt", output, function(err) {
-            if (err) {
-              return console.log(err);
-            }
-          });
+          allOutput += output;
         }
+        fs.appendFile("log.txt", allOutput, function(err) {
+          if (err) {
+            return console.log(err);
+          }
+        });
       }
     });
   },
   //getSpotify is a function that runs the node-spotify-api and returns info on a song entered
   getSpotify: () => {
     if (argStr != '') {
+      argStr = argStr;
+    } else {
+      argStr = "The Sign Ace of Base";
+    }
       keys.spotify.search({type: 'track', query: argStr}, function(err, data) {
         const spot = data.tracks.items[0];
         const output = `-------------------------------\nThe band you're thinking of is:\n${spot.artists[0].name} and they sing:\n${spot.name} which appears on the album:\n${spot.album.name}. Here's a link to the song, if you'd like to check it out:\n${spot.href}\n`;
@@ -46,25 +52,14 @@ let app = {
           }
         });
       });
-    } else {
-      keys.spotify.search({type: 'track', query: 'The Sign Ace of Base'}, function(err, data) {
-        const spot = data.tracks.items[0];
-        const output = `Nothing entered, defaulting to...\n-------------------------------\nThe band you're thinking of is:\n${spot.artists[0].name} and they sing:\n${spot.name} which appears on the album:\n${spot.album.name}. Here's a link to the song, if you'd like to check it out:\n${spot.href}\n`;
-        if (err) {
-          return console.log(err);
-        }
-        console.log(output);
-        fs.appendFile("log.txt", output, function(err) {
-          if (err) {
-            return console.log(err);
-          }
-        });
-      });
-    }
   },
   //getMovie is a function that uses request to make request to the OMBDApi to retrieve info on a movie
   getMovie: () => {
     if (argStr != '') {
+      argStr = argStr;
+    } else {
+      argStr = "Mr. Nobody";
+    }
       ombdApi += "?t=" + argStr + "&y=&plot=short&apikey=40e9cece";
       request(ombdApi, function(error, response, body) {
         if (!error && response.statusCode === 200) {
@@ -78,22 +73,6 @@ let app = {
           });
         }
       });
-    } else {
-      argStr = "Mr. Nobody";
-      ombdApi += "?t=" + argStr + "&y=&plot=short&apikey=40e9cece";
-      request(ombdApi, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-          const info = JSON.parse(body);
-          const output = `Nothing entered, defaulting to...\n-------------------------------\nThis movie's title is: ${info.Title}, which was released in ${info.Year}.\nIMDB scored ${info.Title} at ${info.imdbRating}, but Rotten Tomato's score was: ${info.Ratings[1].Value}.\n${info.Title} was made in this country or countries: ${info.Country}.\nYou can hear ${info.Title} in these languages: ${info.Language}.\nWanna know what ${info.Title} is about?\n${info.Plot}\nFinally, these are the actors: ${info.Actors}\n`;
-          console.log(output);
-          fs.appendFile("log.txt", output, function(err) {
-            if (err) {
-              return console.log(err);
-            }
-          });
-        }
-      });
-    }
   },
   //justDoIt is a function that reads a txt file, extrats the info, stores that info in variables and then executes checkArg()
   justDoIt: () => {
@@ -112,16 +91,16 @@ let app = {
   checkArg: () => {
     switch(_2ndArg) {
       case 'my-tweets':
-        return app.getTweets();
+        app.getTweets();
         break;
       case 'spotify-this-song':
-        return app.getSpotify();
+        app.getSpotify();
         break;
       case 'movie-this':
-        return app.getMovie();
+        app.getMovie();
         break;
       case 'do-what-it-says':
-        return app.justDoIt();
+        app.justDoIt();
         break;
       default:
         console.log('Please enter either "my-tweets", "spotify-this-song", "movie-this", or "do-what-it-says"');
